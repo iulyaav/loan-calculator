b03cd63bd8269be9f8e8d7546041200b4d166a90
Iulia Avram <avram.iulia.augusta@gmail.com>
2020-08-10 23:10:29 +0300

    Add states and some mock calls

--- frontend/loan-frontend/src/components/App.js
+++ frontend/loan-frontend/src/components/App.js
@@ -2,17 +2,47 @@ import './App.css';
 import React from 'react';
 
 import Form from './Form';
+import axios from 'axios';
 
 
 class App extends React.Component {
 
+    state = {
+        amount: 0,
+        monthlyRate: 0.0,
+        payments: 0,
+        interest: 1.0
+    };
+
+    onInputChange = (value) => {
+        this.setState({amount: value});
+    }
+
+    onFormSubmit = async (amount) => {
+        const response = await axios.get('http://localhost:3001/loans', {
+            params: {
+                amount: amount
+            },
+        });
+        console.log(response);
+        this.setState({ amount: 10, monthlyRate: 5.5, payments: 2, interest: 1.5 });
+    }
+
+
     render() {
         return (
-            <div className="container app app-border">
-                <div className="content">
+            <div className="app">
+                <div className="containerr">
                     <h1>Loan Calculator</h1>
                     <hr/>
-                    <Form />
+                    <Form 
+                        onSubmit={this.onFormSubmit}
+                        onInputChange={this.onInputChange}
+                        amount={this.state.amount}
+                        monthlyRate={this.state.monthlyRate}
+                        payments={this.state.payments}
+                        interest={this.state.interest}
+                    />
                 </div>
             </div>
         );
--- frontend/loan-frontend/src/components/Form.js
+++ frontend/loan-frontend/src/components/Form.js
@@ -4,25 +4,35 @@ import React from 'react';
 
 class Form extends React.Component {
 
+    onInputChange = (event) => {
+        this.props.onInputChange(event.target.value);
+    }
+
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(100);
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.onFormSubmit}>
                 <div className="row">
                     <div className="column">
                         <label htmlFor="amount">Loan amount</label>
-                        <input type="text" id="amount"></input>
+                        <input type="text"  value={this.props.amount} onChange={this.onInputChange} id="amount"></input>
                     </div>
                     <div className="column">
                         <label htmlFor="payments">Tenure</label>
-                        <input type="text" placeholder="*No. months" id="payments"></input>
+                        <input type="text"  value={this.props.payments} placeholder="*No. months" id="payments"></input>
                     </div>
                     <div className="column">
                         <label htmlFor="monthlyRate">Monthly rate</label>
-                        <input type="text" id="monthlyRate"></input>
+                        <input type="text"  value={this.props.monthlyRate} id="monthlyRate"></input>
                     </div>
                     <div className="column">
                         <label htmlFor="interest">Interest (our rate)</label>
-                        <input type="text" id="interest" disabled></input>
+                        <input type="text"  value={this.props.interest} id="interest" disabled></input>
                         <p className="under-text">Some text</p>
                     </div>
                 </div>
